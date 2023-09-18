cd9e341ef963a5269386aa8ca6ca4ae7902f4e29
WV1419 <144584323+WV1419@users.noreply.github.com>
2023-09-18 10:07:13 +0530

    Add files via upload

---
+++ unit -1 challenge/leap year.py
@@ -0,0 +1,14 @@
+#Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements.
+
+def isleapYear(year):
+  if(year % 4 == 0 and year % 100 !=0) or year % 400==0:
+    return True
+  else:
+    return False
+
+year = int(input("Enter a year: "))
+
+if isleapYear(year):
+  print('{}is a leap year.'.format(year))
+else:
+  print('{} is not a leap year.'.format(year)) 1
